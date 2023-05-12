4d74d5fe2e0d30b950f9f3ee1b585d934bcd0bb5
ElliTe20C <0411loel@jonkoping.se>
2023-05-12 09:21:45 +0200

    tre övriga sidorna+lite java

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+
+let knappen = document.getElementsByClassName("knapp");
+knappen.addEventListener("click" , myFunktion);
+
+function myFunktion () {
+    knappen.style.backgroundColor = "#fecaf8";
+}
